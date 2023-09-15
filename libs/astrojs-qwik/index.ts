2441a275f2f771644718b55ada69e5359f8c5cda
thejackshelton <me@jackshelton.com>
2023-09-14 23:39:43 -0500

    adding server

--- libs/astrojs-qwik/index.ts
+++ libs/astrojs-qwik/index.ts
@@ -1,33 +1,29 @@
 import type { AstroIntegration, AstroRenderer } from "astro";
 import { qwikVite } from "@builder.io/qwik/optimizer";
-import type { QwikVitePlugin } from "@builder.io/qwik/optimizer";
-import qwikloader from "@builder.io/qwik/qwikloader";
+import type { UserConfig } from "vite";
 
 function getRenderer(): AstroRenderer {
   return {
     name: "@astrojs/qwik",
-    clientEntrypoint: "",
     serverEntrypoint: "@astrojs/qwik/server.js",
   };
 }
 
+async function getViteConfiguration(): Promise<UserConfig> {
+  const config: UserConfig = {
+    plugins: [qwikVite()],
+  };
+
+  return config;
+}
+
 export default function createIntegration(): AstroIntegration {
-  // See the Integration API docs for full details
-  // https://docs.astro.build/en/reference/integrations-reference/
   return {
     name: "@astrojs/qwik",
     hooks: {
-      "astro:config:setup": () => {
-        // See the @astrojs/react integration for an example
-        // https://github.com/withastro/astro/blob/main/packages/integrations/react/src/index.ts
-      },
-      "astro:build:setup": ({ config, updateConfig }) => {
-        // See the @astrojs/netlify integration for an example
-        // https://github.com/withastro/astro/blob/main/packages/integrations/netlify/src/integration-functions.ts
-      },
-      "astro:build:done": ({ dir, routes }) => {
-        // See the @astrojs/partytown integration for an example
-        // https://github.com/withastro/astro/blob/main/packages/integrations/partytown/src/index.ts
+      "astro:config:setup": async ({ addRenderer, updateConfig }) => {
+        addRenderer(getRenderer());
+        updateConfig({ vite: await getViteConfiguration() });
       },
     },
   };
---
+++ libs/astrojs-qwik/server.ts
@@ -0,0 +1,25 @@
+import { h } from "@builder.io/qwik";
+import { renderToString } from "@builder.io/qwik/server";
+
+async function renderToStaticMarkup(Component, props, slotted) {
+  const slots = {};
+  for (const [key, value] of Object.entries(slotted)) {
+    slots[key] = value;
+  }
+  const app = h(Component, { ...props, slots });
+  const html = await renderToString(app);
+  return { html };
+}
+
+export default {
+  renderToStaticMarkup,
+  supportsAstroStaticSlot: true,
+};
+
+/* 
+
+I don't believe we need static-html.js or shouldComponentUpdate, this is because Qwik views components as if they are static already. This is because the listeners are themselves are the roots of the application.
+
+https://www.builder.io/blog/hydration-tree-resumability-map#resumability-is-fundamentally-a-different-algorithm
+
+*/
