4b540c00013be30389d95c875c97285764ce94ce
thejackshelton <me@jackshelton.com>
2023-09-16 16:14:20 -0500

    cleaning up root.tsx

--- libs/astrojs-qwik/root.tsx
+++ libs/astrojs-qwik/root.tsx
@@ -1,13 +1,10 @@
 import { Slot, component$, useSignal } from "@builder.io/qwik";
 
 export default component$(() => {
-  const counter = useSignal(0);
-
   return (
     <div>
       <Slot />
-      I'm inside astrojs-qwik!
-      <button onClick$={() => counter.value++}>{counter.value}</button>
+      I'm inside astrojs-qwik root!
     </div>
   );
 });
