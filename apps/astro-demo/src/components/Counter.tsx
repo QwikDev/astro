ac581390be62ae86178e50c99546e563335b5063
thejackshelton <me@jackshelton.com>
2023-09-18 17:59:30 -0500

    adding proper exports

--- apps/astro-demo/src/components/Counter.tsx
+++ apps/astro-demo/src/components/Counter.tsx
@@ -1,7 +1,17 @@
-import { component$, useSignal } from "@builder.io/qwik";
+// import { component$, useSignal } from "@builder.io/qwik";
 
-export const Counter = component$(() => {
-  const counter = useSignal(0);
+import { componentQrl, inlinedQrl, useSignal } from "@builder.io/qwik";
 
-  return <button onClick$={() => counter.value++}>{counter.value}</button>;
-});
+// export const Counter = component$(() => {
+//   const counter = useSignal(0);
+
+//   return <button onClick$={() => counter.value++}>{counter.value}</button>;
+// });
+
+export const Counter = componentQrl(
+  inlinedQrl(() => {
+    const counter = useSignal(0);
+
+    return <button>{counter.value}</button>;
+  }, "counterqrl")
+);
