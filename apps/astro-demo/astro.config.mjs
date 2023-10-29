dd47f761b85dd641b30d288e02c90c9b101d30c1
thejackshelton <me@jackshelton.com>
2023-10-28 22:36:51 -0500

    back to static adapter

--- apps/astro-demo/astro.config.mjs
+++ apps/astro-demo/astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
-import { qwikVite } from "@builder.io/qwik/optimizer";
 import qwik from "@astrojs/qwik";
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [qwik()],
-  output: "server",
+  output: "static",
   adapter: node({ mode: "standalone" }),
 });
