147f677bae0ddafb3439238f0945ea5dba312fae
thejackshelton <me@jackshelton.com>
2024-02-20 16:39:03 -0600

    remove unnecessary exports

--- libs/qwikdev-astro/src/index.ts
+++ libs/qwikdev-astro/src/index.ts
@@ -16,10 +16,10 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { join, normalize, relative } from "node:path";
 import { rmSync } from "node:fs";
 import os from "os";
-
-// integration files
-import { hash, moveArtifacts } from "@qwikdev/astro/utils";
-import { getQwikEntrypoints } from "@qwikdev/astro/entrypoints";
+import ts from "typescript";
+import fs from "node:fs";
+import { readdir } from "node:fs/promises";
+import fsExtra from "fs-extra";
 
 /* similar to vite's FilterPattern */
 const FilternPatternSchema = z.union([
@@ -196,3 +196,86 @@ export default defineIntegration({
     };
   },
 });
+
+/**
+ *
+ * We need to find the Qwik entrypoints so that the client build will run successfully.
+ *
+ */
+export async function getQwikEntrypoints(
+  dir: string,
+  filter: (id: unknown) => boolean
+): Promise<string[]> {
+  const files = await crawlDirectory(dir);
+  const qwikFiles = [];
+
+  for (const file of files) {
+    // Skip files not matching patterns w/ astro config include & exclude
+    if (!filter(file)) {
+      continue;
+    }
+
+    const fileContent = fs.readFileSync(file, "utf-8");
+    const sourceFile = ts.createSourceFile(
+      file,
+      fileContent,
+      ts.ScriptTarget.ESNext,
+      true
+    );
+
+    let qwikImportFound = false;
+
+    ts.forEachChild(sourceFile, function nodeVisitor(node) {
+      if (
+        ts.isImportDeclaration(node) &&
+        ts.isStringLiteral(node.moduleSpecifier)
+      ) {
+        if (
+          node.moduleSpecifier.text === "@builder.io/qwik" ||
+          node.moduleSpecifier.text === "@builder.io/qwik-react"
+        ) {
+          qwikImportFound = true;
+        }
+      }
+
+      if (!qwikImportFound) {
+        ts.forEachChild(node, nodeVisitor);
+      }
+    });
+
+    if (qwikImportFound) {
+      qwikFiles.push(file);
+    }
+  }
+
+  return qwikFiles;
+}
+
+export function hash() {
+  return Math.random().toString(26).split(".").pop();
+}
+
+export async function moveArtifacts(srcDir: string, destDir: string) {
+  // Ensure the destination dir exists, create if not
+  await fsExtra.ensureDir(destDir);
+  for (const file of await readdir(srcDir)) {
+    // move files from source to destintation, overwrite if they exist
+    await fsExtra.move(join(srcDir, file), join(destDir, file), {
+      overwrite: true,
+    });
+  }
+}
+
+export async function crawlDirectory(dir: string): Promise<string[]> {
+  const entries = await readdir(dir, { withFileTypes: true });
+
+  const files = await Promise.all(
+    entries.map((entry) => {
+      const fullPath = join(dir, entry.name);
+      return entry.isDirectory() ? crawlDirectory(fullPath) : fullPath;
+    })
+  );
+
+  // flatten files array
+  return files.flat();
+}
