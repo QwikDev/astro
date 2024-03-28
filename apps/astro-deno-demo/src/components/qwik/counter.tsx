30af7ce5ca3f48e96e969575a20110f5009f73ae
Sigui Kessé Emmanuel <contact@sigui.ci>
2024-03-28 12:10:12 +0100

    :heavy_plus_sign: Add Deno adapter demo

---
+++ apps/astro-deno-demo/src/components/qwik/counter.tsx
@@ -0,0 +1,13 @@
+import { Slot, component$, useSignal } from "@builder.io/qwik";
+
+export const Counter = component$<{ initial: number }>((props) => {
+  const counter = useSignal(props.initial);
+
+  return (
+    <>
+      <button type="button" onClick$={() => counter.value++}>
+        <Slot /> {counter.value}
+      </button>
+    </>
+  );
+});
