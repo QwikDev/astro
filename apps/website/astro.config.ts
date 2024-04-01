c428a65d8aae3dca59c6cf6fa7f8ef18753a3cc9
Sigui Kessé Emmanuel <contact@sigui.ci>
2024-04-01 07:02:06 +0100

    :tada: Begin the website

---
+++ apps/website/astro.config.ts
@@ -0,0 +1,37 @@
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: true,
+  integrations: [
+    starlight({
+      title: "Qwik + Astro",
+      logo: {
+        src: "./src/assets/qwik-astro.svg",
+        alt: "Qwik + Astro Logo",
+        replacesTitle: true
+      },
+      social: {
+        github: "https://github.com/QwikDev/astro"
+      },
+      editLink: {
+        baseUrl: "https://github.com/QwikDev/astro/edit/main/apps/website/"
+      },
+      sidebar: [
+        {
+          label: "Home",
+          link: "/"
+        },
+        {
+          label: "Guides",
+          items: [{ label: "Overview", link: "/guides/" }]
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" }
+        }
+      ]
+    })
+  ]
+});
