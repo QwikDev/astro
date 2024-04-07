09a04e67d55bd8cb05fe8fa565480155e0a90dbf
Sigui Kessé Emmanuel <contact@sigui.ci>
2024-04-07 02:13:00 +0100

    :green_heart: Configure CI workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,8 +70,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run start",
-    url: "http://127.0.0.1:3000",
-    reuseExistingServer: !process.env.CI
+    command: "pnpm dev",
+    url: "http://localhost:4321",
+    reuseExistingServer: !process.env.CI,
   }
 });
