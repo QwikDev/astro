55858e6f21457510896d66d5e46694be0394b5fa
jack shelton <me@jackshelton.com>
2024-07-30 22:48:38 -0500

    initial starlight setup

--- apps/website/astro.config.ts
+++ apps/website/astro.config.ts
@@ -1,37 +1,39 @@
 import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [
-    starlight({
-      title: "Qwik 💜 Astro",
-      logo: {
-        src: "./src/assets/qwik-astro.svg",
-        alt: "Qwik + Astro Logo",
-        replacesTitle: false
-      },
-      social: {
-        github: "https://github.com/QwikDev/astro"
-      },
-      editLink: {
-        baseUrl: "https://github.com/QwikDev/astro/edit/main/apps/website/"
-      },
-      sidebar: [
-        {
-          label: "Home",
-          link: "/"
-        },
-        {
-          label: "Guides",
-          items: [{ label: "Getting Started", link: "/guides/" }]
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" }
-        }
-      ]
-    })
-  ]
+  integrations: [starlight({
+    title: "Qwik 💜 Astro",
+    customCss: ['./src/tailwind.css'],
+    logo: {
+      src: "./src/assets/qwik-astro.svg",
+      alt: "Qwik + Astro Logo",
+      replacesTitle: false
+    },
+    social: {
+      github: "https://github.com/QwikDev/astro"
+    },
+    editLink: {
+      baseUrl: "https://github.com/QwikDev/astro/edit/main/apps/website/"
+    },
+    sidebar: [{
+      label: "Home",
+      link: "/"
+    }, {
+      label: "Guides",
+      items: [{
+        label: "Getting Started",
+        link: "/guides/"
+      }]
+    }, {
+      label: "Reference",
+      autogenerate: {
+        directory: "reference"
+      }
+    }]
+  }), tailwind({ applyBaseStyles: false })]
 });
