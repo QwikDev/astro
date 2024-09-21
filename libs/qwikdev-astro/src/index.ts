05cd8b55c70db3c2f3deb1f701a2afd878784bd6
jack shelton <me@jackshelton.com>
2024-09-20 20:55:35 -0500

    comment entrypoints

--- libs/qwikdev-astro/src/index.ts
+++ libs/qwikdev-astro/src/index.ts
@@ -1,4 +1,3 @@
-import fs from "node:fs";
 import { rmSync } from "node:fs";
 import os from "node:os";
 import { normalize, relative } from "node:path";
@@ -15,7 +14,6 @@ import { build, createFilter } from "vite";
 import type { InlineConfig, PluginOption } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-import { getQwikEntrypoints, qwikModules } from "./entrypoints";
 import { moveArtifacts, newHash } from "./utils";
 
 declare global {
@@ -35,7 +33,6 @@ const FilterPatternSchema = z.union([
  * This project uses Astro Integration Kit.
  * @see https://astro-integration-kit.netlify.app/
  */
-
 export default defineIntegration({
   name: "@qwikdev/astro",
   optionsSchema: z
@@ -63,7 +60,6 @@ export default defineIntegration({
     const tempDir = `tmp-${newHash()}`;
 
     let astroConfig: AstroConfig | null = null;
-    let entrypoints: Promise<string[]>;
 
     const { resolve: resolver } = createResolver(import.meta.url);
     const filter = createFilter(options?.include, options?.exclude);
@@ -88,89 +84,76 @@ export default defineIntegration({
         // Retrieve Qwik files from the project source directory
         srcDir = relative(astroConfig.root.pathname, astroConfig.srcDir.pathname);
 
-        entrypoints = getQwikEntrypoints(srcDir, filter);
+        addRenderer({
+          name: "@qwikdev/astro",
+          serverEntrypoint: "@qwikdev/astro/server"
+        });
 
-        if ((await entrypoints).length !== 0) {
-          addRenderer({
-            name: "@qwikdev/astro",
-            serverEntrypoint: "@qwikdev/astro/server"
-          });
+        // Update the global dist directory
+        outDir = astroConfig.outDir.pathname;
 
-          // Update the global dist directory
-          outDir = astroConfig.outDir.pathname;
+        // checks all windows platforms and removes drive ex: C:\\
+        if (os.platform() === "win32") {
+          outDir = outDir.substring(3);
+        }
 
-          // checks all windows platforms and removes drive ex: C:\\
-          if (os.platform() === "win32") {
-            outDir = outDir.substring(3);
+        /** We need to get the symbolMapper straight from qwikVite here. You can think of it as the "manifest" for dev mode. */
+        const symbolMapperPlugin: PluginOption = {
+          name: "grabSymbolMapper",
+          configResolved() {
+            globalThis.symbolMapperFn = symbolMapper;
+          }
+        };
+
+        /** check if the file should be processed based on the 'transform' hook and user-defined filters (include & exclude) */
+        const fileFilter = (id: string, hook: string) => {
+          if (hook === "transform" && !filter(id)) {
+            return false;
           }
 
-          /** We need to get the symbolMapper straight from qwikVite here. You can think of it as the "manifest" for dev mode. */
-          const symbolMapperPlugin: PluginOption = {
-            name: "grabSymbolMapper",
-            configResolved() {
-              globalThis.symbolMapperFn = symbolMapper;
-            }
-          };
+          return true;
+        };
 
-          /** check if the file should be processed based on the 'transform' hook and user-defined filters (include & exclude) */
-          const fileFilter = (id: string, hook: string) => {
-            try {
-              const content = fs.readFileSync(id, "utf-8");
-              if (qwikModules.some((module) => content.includes(module))) {
-                return true;
-              }
-            } catch (error) {
-              // file can't be read, silently continue
-            }
+        const qwikViteConfig: QwikVitePluginOptions = {
+          fileFilter,
+          devSsrServer: false,
+          srcDir,
+          client: {
+            input: "src/root.tsx"
+          },
+          ssr: {
+            input: "@qwikdev/astro/server"
+          },
+          debug: options?.debug ?? false
+        };
 
-            if (hook === "transform" && !filter(id)) {
-              return false;
-            }
+        const overrideEsbuildPlugin: PluginOption = {
+          // override qwikVite's attempt to set `esbuild` to false during dev
+          name: "overrideEsbuild",
+          enforce: "post",
+          config(config) {
+            config.esbuild = {};
+            return config;
+          }
+        };
 
-            return true;
-          };
-
-          const qwikViteConfig: QwikVitePluginOptions = {
-            fileFilter,
-            devSsrServer: false,
-            srcDir,
-            client: {
-              input: await entrypoints
-            },
-            ssr: {
-              input: "@qwikdev/astro/server"
-            },
-            debug: options?.debug ?? false
-          };
-
-          const overrideEsbuildPlugin: PluginOption = {
-            // override qwikVite's attempt to set `esbuild` to false during dev
-            name: "overrideEsbuild",
-            enforce: "post",
-            config(config) {
-              config.esbuild = {};
-              return config;
-            }
-          };
-
-          updateConfig({
-            vite: {
-              build: {
-                rollupOptions: {
-                  output: {
-                    inlineDynamicImports: false
-                  }
+        updateConfig({
+          vite: {
+            build: {
+              rollupOptions: {
+                output: {
+                  inlineDynamicImports: false
                 }
-              },
-              plugins: [
-                symbolMapperPlugin,
-                qwikVite(qwikViteConfig),
-                tsconfigPaths(),
-                overrideEsbuildPlugin
-              ]
-            }
-          });
-        }
+              }
+            },
+            plugins: [
+              symbolMapperPlugin,
+              qwikVite(qwikViteConfig),
+              tsconfigPaths(),
+              overrideEsbuildPlugin
+            ]
+          }
+        });
       },
 
       "astro:config:done": async ({ config }) => {
@@ -180,55 +163,52 @@ export default defineIntegration({
       "astro:build:start": async ({ logger }) => {
         logger.info("astro:build:start");
 
-        if ((await entrypoints).length > 0) {
-          // make sure vite does not parse .astro files in the qwik integration
-          const astroNoopPlugin: PluginOption = {
-            enforce: "pre",
-            name: "astro-noop",
+        // make sure vite does not parse .astro files in the qwik integration
+        const astroNoopPlugin: PluginOption = {
+          enforce: "pre",
+          name: "astro-noop",
 
-            load(id: string) {
-              if (id.endsWith(".astro")) {
-                return "export default function() {}";
-              }
-              return null;
+          load(id: string) {
+            if (id.endsWith(".astro")) {
+              return "export default function() {}";
             }
-          };
+            return null;
+          }
+        };
 
-          // client build that we pass back to the server build
-          await build({
-            ...astroConfig?.vite,
-            plugins: [...(astroConfig?.vite.plugins || []), astroNoopPlugin]
-          } as InlineConfig);
+        // client build that we pass back to the server build
+        await build({
+          ...astroConfig?.vite,
+          plugins: [...(astroConfig?.vite.plugins || []), astroNoopPlugin]
+        } as InlineConfig);
 
-          await moveArtifacts(outDir, tempDir);
-        } else {
-          logger.info("No entrypoints found. Skipping build.");
-        }
+        await moveArtifacts(outDir, tempDir);
       },
 
-      "astro:build:done": async ({ logger }) => {
-        if ((await entrypoints).length > 0 && astroConfig) {
-          let outputPath: string;
-          if (astroConfig.output === "server" || astroConfig.output === "hybrid") {
-            outputPath = astroConfig.build.client.pathname;
-          } else {
-            outputPath = astroConfig.outDir.pathname;
-          }
+      "astro:build:done": async () => {
+        let outputPath: string;
 
-          // checks all windows platforms and removes drive ex: C:\\
-          if (os.platform() === "win32") {
-            outputPath = outputPath.substring(3);
-          }
-
-          const normalizedPath = normalize(outputPath);
-          process.env.Q_BASE = normalizedPath;
-
-          await moveArtifacts(tempDir, normalizedPath);
-          // remove the temp dir folder
-          rmSync(tempDir, { recursive: true });
-        } else {
-          logger.info("Build finished. No artifacts moved.");
+        if (!astroConfig) {
+          throw new Error("Qwik Astro: there is no astro config present.");
         }
+
+        if (astroConfig.output === "server" || astroConfig.output === "hybrid") {
+          outputPath = astroConfig.build.client.pathname;
+        } else {
+          outputPath = astroConfig.outDir.pathname;
+        }
+
+        // checks all windows platforms and removes drive ex: C:\\
+        if (os.platform() === "win32") {
+          outputPath = outputPath.substring(3);
+        }
+
+        const normalizedPath = normalize(outputPath);
+        process.env.Q_BASE = normalizedPath;
+
+        await moveArtifacts(tempDir, normalizedPath);
+        // remove the temp dir folder
+        rmSync(tempDir, { recursive: true });
       }
     };
 
