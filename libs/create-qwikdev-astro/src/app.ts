fa3cf5067a6cb21644bc967f6546224eff0b2ca5
Sigui Kessé Emmanuel <siguici@proton.me>
2024-10-16 23:59:14 +0100

    :recycle: Refactor the application's `run` method

--- libs/create-qwikdev-astro/src/app.ts
+++ libs/create-qwikdev-astro/src/app.ts
@@ -140,141 +140,171 @@ export class Application {
       const templatePath = path.join(__dirname, "..", "stubs", "templates", starterKit);
       const outDir: string = resolveAbsoluteDir(projectAnswer.trim());
 
-      log.step(`Creating new project in ${bgBlue(` ${outDir} `)} ... 🐇`);
+      await this.createProject(templatePath, outDir, projectAnswer, adapter);
+      this.copyTemplate(templatePath, outDir);
+      await this.updatePackageJson(projectAnswer, outDir);
+      await this.runCI(outDir);
+      const ranInstall = await this.runInstall(projectAnswer);
+      await this.runGitInit(outDir);
+      this.end(outDir, ranInstall);
+    } catch (err) {
+      console.error("An error occurred during QwikDev/astro project creation:", err);
+      process.exit(1);
+    }
+  }
 
-      if (fs.existsSync(outDir) && fs.readdirSync(outDir).length > 0) {
-        const force = await this.scanForce(outDir);
-        if (force) {
-          if (!this.#config.dryRun) {
-            await clearDir(outDir);
-          }
-        } else {
-          log.error(`Directory "${outDir}" already exists.`);
-          log.info(
-            `Please either remove this directory, choose another location or run the command again with '--force | -f' flag.`
-          );
-          cancel();
-          process.exit(1);
+  async createProject(
+    templatePath: string,
+    outDir: string,
+    projectAnswer: string,
+    adapter: Adapter
+  ): Promise<void> {
+    log.step(`Creating new project in ${bgBlue(` ${outDir} `)} ... 🐇`);
+
+    if (fs.existsSync(outDir) && fs.readdirSync(outDir).length > 0) {
+      const force = await this.scanForce(outDir);
+      if (force) {
+        if (!this.#config.dryRun) {
+          await clearDir(outDir);
         }
+      } else {
+        log.error(`Directory "${outDir}" already exists.`);
+        log.info(
+          `Please either remove this directory, choose another location or run the command again with '--force | -f' flag.`
+        );
+        cancel();
+        process.exit(1);
       }
+    }
+  }
+
+  async updatePackageJson(projectAnswer: string, outDir: string) {
+    const defaultPackageName = sanitizePackageName(projectAnswer);
+    const packageName = await this.scanString(
+      "What should be the name of this package?",
+      defaultPackageName
+    );
+
+    updatePackageName(packageName, outDir);
+    log.info(`Updated package name to "${packageName}" 📦️`);
+
+    if (getPackageManager() !== "npm") {
+      log.info(`Replacing 'npm run' by '${pmRunCommand()}' in package.json...`);
+      replacePackageJsonRunCommand(outDir);
+    }
+  }
+
+  async runCI(outDir: string): Promise<void> {
+    const ci = await this.scanCI();
+
+    if (ci) {
+      log.step("Adding CI workflow...");
 
       if (!this.#config.dryRun) {
-        if (!existsSync(outDir)) {
-          mkdirSync(outDir, { recursive: true });
-        }
-        cpSync(templatePath, outDir, { recursive: true });
-      }
-
-      const defaultPackageName = sanitizePackageName(projectAnswer);
-      const packageName = await this.scanString(
-        "What should be the name of this package?",
-        defaultPackageName
-      );
-
-      updatePackageName(packageName, outDir);
-      log.info(`Updated package name to "${packageName}" 📦️`);
-
-      if (packageManager !== "npm") {
-        log.info(`Replacing 'npm run' by '${pmRunCommand()}' in package.json...`);
-        replacePackageJsonRunCommand(outDir);
-      }
-
-      const addCIWorkflow = await this.scanCI();
-
-      if (addCIWorkflow && !this.#config.dryRun) {
         const starterCIPath = path.join(
           __dirname,
           "..",
           "stubs",
           "workflows",
           `${
-            ["npm", "yarn", "pnpm", "bun"].includes(packageManager)
-              ? packageManager
+            ["npm", "yarn", "pnpm", "bun"].includes(getPackageManager())
+              ? getPackageManager()
               : "npm"
           }-ci.yml`
         );
         const projectCIPath = path.join(outDir, ".github", "workflows", "ci.yml");
         cpSync(starterCIPath, projectCIPath, { force: true });
       }
-
-      const runInstall = await this.scanInstall();
-
-      let ranInstall = false;
-      if (typeof runInstall !== "symbol" && runInstall) {
-        log.step("Installing dependencies...");
-        if (!this.#config.dryRun) {
-          await installDependencies(projectAnswer);
-        }
-        ranInstall = true;
-      }
-
-      const initGit = await this.scanGit();
-
-      if (initGit) {
-        const s = spinner();
-
-        if (fs.existsSync(path.join(outDir, ".git"))) {
-          log.info("Git has already been initialized before. Skipping...");
-        } else {
-          s.start("Git initializing...");
-
-          try {
-            if (!this.#config.dryRun) {
-              const res = [];
-              res.push(await $("git", ["init"], outDir).install);
-              res.push(await $("git", ["add", "-A"], outDir).install);
-              res.push(
-                await $("git", ["commit", "-m", "Initial commit 🎉"], outDir).install
-              );
-
-              if (res.some((r) => r === false)) {
-                throw "";
-              }
-            }
-
-            s.stop("Git initialized 🎲");
-          } catch (e) {
-            s.stop("Git failed to initialize");
-            log.error(
-              red(
-                "Git failed to initialize. You can do this manually by running: git init"
-              )
-            );
-          }
-        }
-      }
-
-      const isCwdDir = process.cwd() === outDir;
-      const relativeProjectPath = resolveRelativeDir(outDir);
-      const outString = [];
-
-      if (isCwdDir) {
-        outString.push(`🦄 ${bgMagenta(" Success! ")}`);
-      } else {
-        outString.push(
-          `🦄 ${bgMagenta(" Success! ")} ${cyan("Project created in")} ${bold(
-            magenta(relativeProjectPath)
-          )} ${cyan("directory")}`
-        );
-      }
-      outString.push("");
-
-      outString.push(`🐰 ${cyan("Next steps:")}`);
-      if (!isCwdDir) {
-        outString.push(`   cd ${relativeProjectPath}`);
-      }
-      if (!ranInstall) {
-        outString.push(`   ${packageManager} install`);
-      }
-      outString.push(`   ${packageManager} start`);
-
-      note(outString.join("\n"), "Ready to start 🚀");
-
-      outro("Happy coding! 💻🎉");
-    } catch (err) {
-      console.error("An error occurred during QwikDev/astro project creation:", err);
-      process.exit(1);
     }
+  }
+
+  async runInstall(projectAnswer: string): Promise<boolean> {
+    const runInstall = await this.scanInstall();
+
+    let ranInstall = false;
+    if (typeof runInstall !== "symbol" && runInstall) {
+      log.step("Installing dependencies...");
+      if (!this.#config.dryRun) {
+        await installDependencies(projectAnswer);
+      }
+      ranInstall = true;
+    }
+
+    return ranInstall;
+  }
+
+  async runGitInit(outDir: string): Promise<void> {
+    const initGit = await this.scanGit();
+    if (initGit) {
+      const s = spinner();
+
+      if (fs.existsSync(path.join(outDir, ".git"))) {
+        log.info("Git has already been initialized before. Skipping...");
+      } else {
+        s.start("Git initializing...");
+
+        try {
+          if (!this.#config.dryRun) {
+            const res = [];
+            res.push(await $("git", ["init"], outDir).install);
+            res.push(await $("git", ["add", "-A"], outDir).install);
+            res.push(
+              await $("git", ["commit", "-m", "Initial commit 🎉"], outDir).install
+            );
+
+            if (res.some((r) => r === false)) {
+              throw "";
+            }
+          }
+
+          s.stop("Git initialized 🎲");
+        } catch (e) {
+          s.stop("Git failed to initialize");
+          log.error(
+            red("Git failed to initialize. You can do this manually by running: git init")
+          );
+        }
+      }
+    }
+  }
+
+  copyTemplate(templatePath: string, outDir: string): void {
+    if (!this.#config.dryRun) {
+      if (!existsSync(outDir)) {
+        mkdirSync(outDir, { recursive: true });
+      }
+      cpSync(templatePath, outDir, { recursive: true });
+    }
+  }
+
+  end(outDir: string, ranInstall: boolean): void {
+    const isCwdDir = process.cwd() === outDir;
+    const relativeProjectPath = resolveRelativeDir(outDir);
+    const outString = [];
+
+    if (isCwdDir) {
+      outString.push(`🦄 ${bgMagenta(" Success! ")}`);
+    } else {
+      outString.push(
+        `🦄 ${bgMagenta(" Success! ")} ${cyan("Project created in")} ${bold(
+          magenta(relativeProjectPath)
+        )} ${cyan("directory")}`
+      );
+    }
+    outString.push("");
+
+    outString.push(`🐰 ${cyan("Next steps:")}`);
+    if (!isCwdDir) {
+      outString.push(`   cd ${relativeProjectPath}`);
+    }
+    if (!ranInstall) {
+      outString.push(`   ${getPackageManager()} install`);
+    }
+    outString.push(`   ${getPackageManager()} start`);
+
+    note(outString.join("\n"), "Ready to start 🚀");
+
+    outro("Happy coding! 💻🎉");
   }
 }
 
