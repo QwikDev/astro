0132d247d74c3e3ea5042744ba06dc655f57e2ac
jack shelton <me@jackshelton.com>
2024-11-27 22:16:28 -0600

    type button

--- libs/qwikdev-astro/tests/fixtures/minimal/src/components/counter.tsx
+++ libs/qwikdev-astro/tests/fixtures/minimal/src/components/counter.tsx
@@ -3,5 +3,5 @@ import { component$, useSignal } from "@builder.io/qwik";
 export const Counter = component$(() => {
   const count = useSignal(0);
 
-  return <button onClick$={() => count.value++}>{count.value}</button>;
+  return <button type="button" onClick$={() => count.value++}>{count.value}</button>;
 });
