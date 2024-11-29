c370247912abf1530be23e74b1e4645ae2b6e29e
Jack Shelton <104264123+thejackshelton@users.noreply.github.com>
2024-11-29 14:33:27 -0600

    use pnpm instead of make fil

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "make dev",
+    command: "pnpm dev",
     url: "http://localhost:4321",
     reuseExistingServer: !process.env.CI
   }
