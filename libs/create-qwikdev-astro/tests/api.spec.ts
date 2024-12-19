98bdf79a348e99ecfdc83eea1744133e04201433
Sigui Kessé Emmanuel <siguici@proton.me>
2024-12-19 20:11:57 +0100

    :white_check_mark: Test arguments and options

--- libs/create-qwikdev-astro/tests/api.spec.ts
+++ libs/create-qwikdev-astro/tests/api.spec.ts
@@ -52,4 +52,73 @@ test.group(`${name}@${version} API testing`, () => {
     assert.isTrue(definition.get("add").isUndefined());
     assert.isTrue(definition.get("add").equals(defaultDefinition.add));
   });
+
+  test("arguments", ({ assert }) => {
+    let definition = tester.parse([]);
+    assert.isTrue(definition.get("destination").isString());
+    assert.isTrue(definition.get("destination").equals("."));
+    assert.isTrue(definition.get("adapter").isUndefined());
+
+    definition = tester.parse(["qapp"]);
+    assert.isTrue(definition.get("destination").isString());
+    assert.isTrue(definition.get("destination").equals("qapp"));
+    assert.isTrue(definition.get("adapter").isUndefined());
+
+    definition = tester.parse(["my-qwik-astro-app", "node"]);
+    assert.isTrue(definition.get("destination").equals("my-qwik-astro-app"));
+    assert.isTrue(definition.get("adapter").isString());
+    assert.isTrue(definition.get("adapter").equals("node"));
+
+    definition = tester.parse(["my-qwik-astro-app", "deno"]);
+    assert.isTrue(definition.get("adapter").isString());
+    assert.isTrue(definition.get("adapter").equals("deno"));
+  });
+
+  test("yes", ({ assert }) => {
+    const definition = tester.parse(["--yes"]);
+    assert.isTrue(definition.get("yes").isBoolean());
+    assert.isTrue(definition.get("yes").isTrue());
+  });
+
+  test("no", ({ assert }) => {
+    const definition = tester.parse(["--no"]);
+    assert.isTrue(definition.get("no").isBoolean());
+    assert.isTrue(definition.get("no").isTrue());
+  });
+
+  test("add", ({ assert }) => {
+    const definition = tester.parse(["--add"]);
+    assert.isTrue(definition.get("add").isBoolean());
+    assert.isTrue(definition.get("add").isTrue());
+  });
+
+  test("force", ({ assert }) => {
+    const definition = tester.parse(["--force"]);
+    assert.isTrue(definition.get("force").isBoolean());
+    assert.isTrue(definition.get("force").isTrue());
+  });
+
+  test("install", ({ assert }) => {
+    const definition = tester.parse(["--install"]);
+    assert.isTrue(definition.get("install").isBoolean());
+    assert.isTrue(definition.get("install").isTrue());
+  });
+
+  test("biome", ({ assert }) => {
+    const definition = tester.parse(["--biome"]);
+    assert.isTrue(definition.get("biome").isBoolean());
+    assert.isTrue(definition.get("biome").isTrue());
+  });
+
+  test("git", ({ assert }) => {
+    const definition = tester.parse(["--git"]);
+    assert.isTrue(definition.get("git").isBoolean());
+    assert.isTrue(definition.get("git").isTrue());
+  });
+
+  test("ci", ({ assert }) => {
+    const definition = tester.parse(["--ci"]);
+    assert.isTrue(definition.get("ci").isBoolean());
+    assert.isTrue(definition.get("ci").isTrue());
+  });
 });
