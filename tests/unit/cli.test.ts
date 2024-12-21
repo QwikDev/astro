4c34ff47bd44e0a39b357cf65f5ef9e70c3eab9e
Sigui Kessé Emmanuel <siguici@proton.me>
2024-12-21 23:27:02 +0100

    :white_check_mark: Test the application execution

--- tests/unit/cli.test.ts
+++ tests/unit/cli.test.ts
@@ -1,9 +1,31 @@
+import { mkdirSync, rmSync } from "node:fs";
+import { afterEach } from "node:test";
 import app from "@qwikdev/create-astro/app";
-import { describe, expect, it } from "vitest";
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest";
+
+const rootDir = "tests/apps";
+const projectName = "test-app";
+const cwd = process.cwd();
+
+beforeAll(() => {
+  mkdirSync(rootDir, { recursive: true });
+});
+
+beforeEach(() => {
+  process.chdir(rootDir);
+});
+
+afterEach(() => {
+  process.chdir(cwd);
+});
+
+afterAll(() => {
+  rmSync(rootDir, { recursive: true, force: true });
+});
 
 describe(`${app.name}@${app.version}`, () => {
   it("should create a new app", async () => {
-    const result = await app.run(["pnpm", "create", "my-qwik-astro-app"]);
+    const result = await app.run(["pnpm", "create", projectName]);
 
     expect(result).toBe(0);
   });
