2d74e06e3dc6c93a80c71e2b3529c5ab9d73d314
jack shelton <me@jackshelton.com>
2024-12-25 23:30:30 -0600

    better interactivity

--- apps/website/src/components/home/cli-copy/cli-copy.tsx
+++ apps/website/src/components/home/cli-copy/cli-copy.tsx
@@ -10,6 +10,7 @@ import styles from "./cli-copy.css?inline";
 
 export const CLICopy = component$((props: PropsOf<"button">) => {
   const isCopied = useSignal(false);
+  const hasInteracted = useSignal(false);
   useStyles$(styles);
 
   useTask$(({ track }) => {
@@ -29,8 +30,10 @@ export const CLICopy = component$((props: PropsOf<"button">) => {
       onClick$={() => {
         navigator.clipboard.writeText("npm create @qwikdev/astro@latest");
         isCopied.value = true;
+        hasInteracted.value = true;
       }}
       data-click-success={isCopied.value ? "true" : "false"}
+      data-interacted={hasInteracted.value ? "" : undefined}
       {...props}
     >
       <span>{isCopied.value ? "Copied!" : "Copy CLI command"}</span>
