af755466e4d827d3d914e658e347be6ce65e8718
thejackshelton <me@jackshelton.com>
2025-01-14 10:35:56 -0600

    add in the squiggles

---
+++ apps/website/src/components/home/squiggle/squiggle.tsx
@@ -0,0 +1,12 @@
+import { component$, useStylesScoped$, type PropsOf } from "@builder.io/qwik"
+import styles from "./squiggle.css?inline"
+
+export const Squiggle = component$((props: PropsOf<"svg">) => {
+    useStylesScoped$(styles)
+
+    return (
+        <svg width="587" height="31" viewBox="0 0 587 31" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path d="M2.50147 10.4772C140.69 52.2251 165.072 6.01877 246.585 19.0008C328.098 31.9828 327.794 -2.77817 421.562 7.01197C496.576 14.8441 561.181 7.78511 584.107 3.27661" stroke={props.stroke ?? "#867CD8"} stroke-width={props['stroke-width'] ?? 3} stroke-linecap="round"/>
+        </svg>
+    )
+});
