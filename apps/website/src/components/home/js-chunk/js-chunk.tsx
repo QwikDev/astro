fcb57be067dcf11c7eac90fe8e01ca694c45fd2d
thejackshelton <me@jackshelton.com>
2025-01-14 21:55:42 -0600

    file cleanup

--- apps/website/src/components/home/js-chunk/js-chunk.tsx
+++ apps/website/src/components/home/js-chunk/js-chunk.tsx
@@ -3,24 +3,23 @@ import { component$, useStylesScoped$ } from "@builder.io/qwik";
 export const JSChunk = component$(() => {
   useStylesScoped$(
     `
-          div {
-            width: 30px;
-            height: 30px;
-            color: var(--off-black);
-            background: #F1DC4E;
-            font-family: system-ui, sans-serif;
-            font-weight: 700;
-            display: flex;
-            align-items: end;
-            justify-content: end;
-            padding-right: 3px;
-          }
+      div {
+        width: 30px;
+        height: 30px;
+        color: var(--off-black);
+        background: #F1DC4E;
+        font-family: system-ui, sans-serif;
+        font-weight: 700;
+        display: flex;
+        align-items: end;
+        justify-content: end;
+        padding-right: 3px;
+      }
 
-          span {
-            display: block;
-          }
-           
-        `,
+      span {
+        display: block;
+      }
+    `,
   );
 
   return (
