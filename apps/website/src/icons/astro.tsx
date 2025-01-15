203657dfc85f943110225e38f7d6b2c58c98b864
thejackshelton <me@jackshelton.com>
2025-01-15 00:39:25 -0600

    add in overshoot

--- apps/website/src/icons/astro.tsx
+++ apps/website/src/icons/astro.tsx
@@ -5,6 +5,7 @@ export const AstroIcon = component$((props: PropsOf<"svg">) => {
     <svg
       aria-describedby="astro-tooltip"
       width="50"
+      height="50"
       viewBox="0 0 85 107"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
