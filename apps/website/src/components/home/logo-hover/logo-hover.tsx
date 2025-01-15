36e0cda288dde8cdd9f38c3ac4aa4d7d7429192c
thejackshelton <me@jackshelton.com>
2025-01-14 23:53:45 -0600

    logos moving

--- apps/website/src/components/home/logo-hover/logo-hover.tsx
+++ apps/website/src/components/home/logo-hover/logo-hover.tsx
@@ -11,58 +11,56 @@ export const LogoHover = component$(() => {
 
   return (
     <>
-      <div class="intro">
-        <h1>
-          <span
-            onMouseEnter$={$(() => {
-              if (!qwikLogoRef.value) return;
-              qwikLogoRef.value.style.opacity = "1";
-              qwikLogoRef.value.style.scale = "1";
-            })}
-            onMouseMove$={$(({ clientX, clientY }) => {
-              if (!qwikLogoRef.value) return;
-              qwikLogoRef.value.style.transform = `translate3d(${clientX}px, ${clientY}px, 0)`;
-            })}
-            onMouseLeave$={$(() => {
-              if (!qwikLogoRef.value) return;
-              qwikLogoRef.value.style.opacity = "0";
-              qwikLogoRef.value.style.scale = "0";
-            })}
-          >
-            {"QWIK".split("").map((letter, i) => (
-              <span class="letter" key={letter} style={`animation-delay: ${i * 0.15}s;`}>
-                {letter}
-              </span>
-            ))}
-          </span>
-          <span
-            onMouseEnter$={$(() => {
-              if (!astroLogoRef.value) return;
-              astroLogoRef.value.style.opacity = "1";
-              astroLogoRef.value.style.scale = "1";
-            })}
-            onMouseMove$={$(({ clientX, clientY }) => {
-              if (!astroLogoRef.value) return;
-              astroLogoRef.value.style.transform = `translate3d(${clientX}px, ${clientY}px, 0)`;
-            })}
-            onMouseLeave$={$(() => {
-              if (!astroLogoRef.value) return;
-              astroLogoRef.value.style.opacity = "0";
-              astroLogoRef.value.style.scale = "0";
-            })}
-          >
-            {"ASTRO".split("").map((letter, i) => (
-              <span
-                class="letter"
-                key={letter}
-                style={`animation-delay: ${(i + 5) * 0.15}s;`}
-              >
-                {letter}
-              </span>
-            ))}
-          </span>
-        </h1>
-      </div>
+      <h1>
+        <span
+          onMouseEnter$={$(() => {
+            if (!qwikLogoRef.value) return;
+            qwikLogoRef.value.style.opacity = "1";
+            qwikLogoRef.value.style.scale = "1";
+          })}
+          onMouseMove$={$(({ clientX, clientY }) => {
+            if (!qwikLogoRef.value) return;
+            qwikLogoRef.value.style.transform = `translate3d(${clientX}px, ${clientY}px, 0)`;
+          })}
+          onMouseLeave$={$(() => {
+            if (!qwikLogoRef.value) return;
+            qwikLogoRef.value.style.opacity = "0";
+            qwikLogoRef.value.style.scale = "0";
+          })}
+        >
+          {"QWIK".split("").map((letter, i) => (
+            <span class="letter" key={letter} style={`animation-delay: ${i * 0.15}s;`}>
+              {letter}
+            </span>
+          ))}
+        </span>
+        <span
+          onMouseEnter$={$(() => {
+            if (!astroLogoRef.value) return;
+            astroLogoRef.value.style.opacity = "1";
+            astroLogoRef.value.style.scale = "1";
+          })}
+          onMouseMove$={$(({ clientX, clientY }) => {
+            if (!astroLogoRef.value) return;
+            astroLogoRef.value.style.transform = `translate3d(${clientX}px, ${clientY}px, 0)`;
+          })}
+          onMouseLeave$={$(() => {
+            if (!astroLogoRef.value) return;
+            astroLogoRef.value.style.opacity = "0";
+            astroLogoRef.value.style.scale = "0";
+          })}
+        >
+          {"ASTRO".split("").map((letter, i) => (
+            <span
+              class="letter"
+              key={letter}
+              style={`animation-delay: ${(i + 5) * 0.15}s;`}
+            >
+              {letter}
+            </span>
+          ))}
+        </span>
+      </h1>
       <QwikIcon ref={qwikLogoRef} class="qwik-logo tooltip" />
       <AstroIcon ref={astroLogoRef} class="astro-logo tooltip" />
     </>
