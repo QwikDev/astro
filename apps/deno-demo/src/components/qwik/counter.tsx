c0ca6cc824b3e17ac366cd75758c404a6f6345f6
thejackshelton <me@jackshelton.com>
2025-03-08 18:03:37 -0600

    deno and node to v2

--- apps/deno-demo/src/components/qwik/counter.tsx
+++ apps/deno-demo/src/components/qwik/counter.tsx
@@ -1,4 +1,4 @@
-import { Slot, component$, useSignal } from "@builder.io/qwik";
+import { Slot, component$, useSignal } from "@qwik.dev/core";
 
 export const Counter = component$<{ initial: number }>((props) => {
   const counter = useSignal(props.initial);
