51feb1fea78152e98fea37e6c7a3545a83f45678
Shane-Donlon <130906067+Shane-Donlon@users.noreply.github.com>
2025-03-25 23:05:55 +0000

    revert to copy static files

--- libs/qwikdev-astro/src/index.ts
+++ libs/qwikdev-astro/src/index.ts
@@ -92,7 +92,10 @@ export default defineIntegration({
         /** Relative paths, as the Qwik optimizer handles normalization */
         srcDir = getRelativePath(astroConfig.root.pathname, astroConfig.srcDir.pathname);
 
-        clientDir = getRelativePath(astroConfig.root.pathname, "dist/");
+        clientDir = getRelativePath(
+          astroConfig.root.pathname,
+          astroConfig.build.client.pathname
+        );
 
         serverDir = getRelativePath(
           astroConfig.root.pathname,
@@ -222,6 +225,14 @@ export default defineIntegration({
       "astro:build:setup": async ({ vite }) => {
         astroVite = vite as InlineConfig;
       },
+      "astro:build:generated"(options) {
+        if (
+          astroConfig?.adapter?.name.includes("vercel") &&
+          fs.existsSync("dist/client/build")
+        ) {
+          copyFolderSync("dist/client/build", "dist/build");
+        }
+      },
 
       "astro:build:ssr": async () => {
         await entrypointsReady;
@@ -320,4 +331,23 @@ export default defineIntegration({
 
 function getRelativePath(from: string, to: string) {
   return to.replace(from, "") || ".";
+}
+
+function copyFolderSync(src: string, dest: string) {
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest, { recursive: true });
+  }
+
+  const entries = fs.readdirSync(src, { withFileTypes: true });
+
+  for (const entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+
+    if (entry.isDirectory()) {
+      copyFolderSync(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
 }
