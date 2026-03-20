70ce6cf155553adcb31443563eb70176dadc2cd5
thejackshelton <me@jackshelton.com>
2026-03-20 02:55:46 -0500

    feat: update intro sequence

--- apps/website/src/components/home/logo-hover/logo-hover.tsx
+++ apps/website/src/components/home/logo-hover/logo-hover.tsx
@@ -53,38 +53,30 @@ export const LogoHover = component$(() => {
     <div class="logo-hover">
       <h1>
         <span
+          class="word"
+          style="animation-delay: 0s;"
           onMouseEnter$={() => handleMouseEnter(qwikLogoRef, qwikRect)}
           onMouseMove$={({ clientX, clientY }) =>
             handleMouseMove(qwikLogoRef, qwikRect, clientX, clientY)
           }
           onMouseLeave$={() => handleMouseLeave(qwikLogoRef)}
         >
-          {"QWIK".split("").map((letter, i) => (
-            <span class="letter" key={letter} style={`animation-delay: ${i * 0.15}s;`}>
-              {letter}
-            </span>
-          ))}
+          QWIK
         </span>
-        <span class="letter plus" style="animation-delay: 0.6s;">+</span>
+        <span class="word" style="animation-delay: 0.7s;">+</span>
         <span
+          class="word"
+          style="animation-delay: 1.4s;"
           onMouseEnter$={() => handleMouseEnter(astroLogoRef, astroRect)}
           onMouseMove$={({ clientX, clientY }) =>
             handleMouseMove(astroLogoRef, astroRect, clientX, clientY)
           }
           onMouseLeave$={() => handleMouseLeave(astroLogoRef)}
         >
-          {"ASTRO".split("").map((letter, i) => (
-            <span
-              class="letter"
-              key={letter}
-              style={`animation-delay: ${(i + 6) * 0.15}s;`}
-            >
-              {letter}
-            </span>
-          ))}
+          ASTRO
         </span>
-        <span class="letter equals" style="animation-delay: 1.65s;">=</span>
-        <span class="letter logo-reveal" style="animation-delay: 1.8s;" dangerouslySetInnerHTML={qwikAstroLogo} />
+        <span class="word" style="animation-delay: 2.1s;">=</span>
+        <span class="logo-reveal" style="animation-delay: 3.2s;" dangerouslySetInnerHTML={qwikAstroLogo} />
       </h1>
       <QwikIcon ref={qwikLogoRef} class="qwik-logo tooltip" />
       <AstroIcon ref={astroLogoRef} class="astro-logo tooltip" />
