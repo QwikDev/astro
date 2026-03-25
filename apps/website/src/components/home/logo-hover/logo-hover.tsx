c2581b684ee834851df41c9338f3231eacfe4d2b
Jack Shelton <104264123+thejackshelton@users.noreply.github.com>
2026-03-25 18:53:43 -0500

    Hero pattern (#282)
    * feat: change hero image
    
    * pixel hero
    
    * image
    
    * hero pattern
    
    * feat: remove pixel desk

--- apps/website/src/components/home/logo-hover/logo-hover.tsx
+++ apps/website/src/components/home/logo-hover/logo-hover.tsx
@@ -49,7 +49,7 @@ export const LogoHover = component$(() => {
       <h1>
         <span
           class="word"
-          style="animation-delay: 0s;"
+          style="animation-delay: 0s; opacity: 0;"
           onMouseEnter$={() => handleMouseEnter(qwikLogoRef, qwikRect)}
           onMouseMove$={({ clientX, clientY }) =>
             handleMouseMove(qwikLogoRef, qwikRect, clientX, clientY)
@@ -58,12 +58,12 @@ export const LogoHover = component$(() => {
         >
           QWIK
         </span>
-        <span class="word" style="animation-delay: 0.4s;">
+        <span class="word" style="animation-delay: 0.4s; opacity: 0;">
           +
         </span>
         <span
           class="word"
-          style="animation-delay: 0.8s;"
+          style="animation-delay: 0.8s; opacity: 0;"
           onMouseEnter$={() => handleMouseEnter(astroLogoRef, astroRect)}
           onMouseMove$={({ clientX, clientY }) =>
             handleMouseMove(astroLogoRef, astroRect, clientX, clientY)
@@ -72,12 +72,12 @@ export const LogoHover = component$(() => {
         >
           ASTRO
         </span>
-        <span class="word" style="animation-delay: 1.2s;">
+        <span class="word" style="animation-delay: 1.2s; opacity: 0;">
           =
         </span>
         <span
           class="logo-reveal"
-          style="animation-delay: 2.2s;"
+          style="animation-delay: 2.2s; opacity: 0;"
           dangerouslySetInnerHTML={qwikAstroLogo}
         />
       </h1>
