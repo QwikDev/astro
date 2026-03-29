ff8d80646f3fb5ed2bfd3ce2df32c32884540237
thejackshelton <me@jackshelton.com>
2026-03-29 00:46:54 -0500

    fix: og image url

--- apps/website/astro.config.ts
+++ apps/website/astro.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro.qwik.dev",
   prefetch: {
     prefetchAll: true,
     defaultStrategy: "viewport",
